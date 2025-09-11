85ce310bf1dd0e67bcf2fc8410c8b5e5af6b1e1c
Myriad-Dreamin <camiyoru@gmail.com>
2025-09-11 08:37:08 +0800

    feat: implement package publishing

--- src/commands/host.rs
+++ src/commands/host.rs
@@ -1,6 +1,10 @@
+use std::io::BufWriter;
 use std::path::Path;
 
+use anyhow::Context;
 use clap::Parser;
+use ecow::EcoString;
+use serde::{Deserialize, Serialize};
 
 const LONG_ABOUT: &str = "Host the package in a GitHub repository.";
 
@@ -9,6 +13,10 @@ const LONG_ABOUT: &str = "Host the package in a GitHub repository.";
 /// Host the package in a GitHub repository
 pub struct HostArgs {
     #[arg(long)]
+    /// The packages to host.
+    /// Space-separated list of packages to host.
+    pub packages: Option<String>,
+    #[arg(long)]
     /// The forked repository from typst/packages.
     pub source: Option<String>,
     #[arg(long)]
@@ -16,47 +24,110 @@ pub struct HostArgs {
     pub destination: Option<String>,
 }
 
+// if: ${{ fromJson(needs.plan.outputs.val).ci.github.artifacts_matrix.include
+// != null && (needs.plan.outputs.publishing == 'true' ||
+// fromJson(needs.plan.outputs.val).ci.github.pr_run_mode == 'upload') }}
+
+// runs-on: ${{ matrix.runner }}
+// container: ${{ matrix.container && matrix.container.image || null }}
+// mkdir -p typst-packages/${{ inputs.destination }}/${{ matrix.package.name }}
+// mv ${{ matrix.package.source }} typst-packages/${{ inputs.destination }}/${{
+// matrix.package.name }}/${{ matrix.package.version }} name: preview-${{
+// matrix.package.name }}-${{ matrix.package.version }} path: typst-packages/${{
+// inputs.destination }}/${{ matrix.package.name }}/${{ matrix.package.version
+// }} title: "@preview/{{ matrix.package.name }} {{ matrix.package.version }}"
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Output {
+    ci: Ci,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Ci {
+    github: CiGitHub,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct CiGitHub {
+    artifacts_matrix: Option<Vec<ArtifactsMatrix>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ArtifactsMatrix {
+    runner: String,
+    container: Option<Container>,
+    package: Package,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Container {
+    image: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Package {
+    source: String,
+    name: EcoString,
+    version: String,
+}
+
 pub fn host(_current_dir: &Path, _args: &HostArgs) -> anyhow::Result<()> {
-    println!(
-        r###"
-      - name: Put package to @preview namespace
-        run: |
-          mkdir -p typst-packages/${{ inputs.destination }}/example
-          mv tests/fixtures/example typst-packages/${{ inputs.destination }}/example/0.1.1
-      - name: Upload built package
-        uses: actions/upload-artifact@v4
-        with:
-          name: preview-example-0.1.1
-          path: typst-packages/${{ inputs.destination }}/example/0.1.1
-      - name: Make PR
-        uses: peter-evans/create-pull-request@v7
-        with:
-          token: ${{ secrets.REGISTRY_TOKEN }}
-          path: typst-packages
-          title: "@preview/example 0.1.1"
-          body: |
-            I am submitting
-            * [ ]  a new package
-            * [x]  an update for a package
+    let packages = _args.packages.as_deref().unwrap_or("all");
+    let mut packages = packages.split_whitespace().collect::<Vec<_>>();
+    if packages == ["all"] {
+        packages = vec![];
+    }
 
-            Description: New release.
+    // find packages in workspace
+    let package_dirs = ignore::WalkBuilder::new(".")
+        .standard_filters(true)
+        .add_custom_ignore_filename(".typstignore")
+        .build()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            (entry.path().file_name()? == "typst.toml").then_some(entry)
+        })
+        .collect::<Vec<_>>();
 
-            I have read and followed the submission guidelines and, in particular, I
+    let mut tasks = vec![];
 
-            * [x]  selected [a name](https://github.com/typst/packages/blob/main/docs/manifest.md#naming-rules) that isn't the most obvious or canonical name for what the package does
-            * [x]  added a [`typst.toml`](https://github.com/typst/packages/blob/main/docs/manifest.md#package-metadata) file with all required keys
-            * [x]  added a [`README.md`](https://github.com/typst/packages/blob/main/docs/documentation.md) with documentation for my package
-            * [x]  have chosen [a license](https://github.com/typst/packages/blob/main/docs/licensing.md) and added a `LICENSE` file or linked one in my `README.md`
-            * [x]  tested my package locally on my system and it worked
-            * [x]  [`exclude`d](https://github.com/typst/packages/blob/main/docs/tips.md#what-to-commit-what-to-exclude) PDFs or README images, if any, but not the LICENSE
+    for package in package_dirs {
+        // package.path().parent().ok_or_else()
+        let package_dir = package.path().parent().ok_or_else(|| {
+            anyhow::anyhow!("Package directory not found: {}", package.path().display())
+        })?;
+        let manifest = crate::utils::read_manifest(package_dir).with_context(|| {
+            format!(
+                "failed to read the package manifest file: {}",
+                package_dir.display()
+            )
+        })?;
+        if !packages.is_empty() && !packages.contains(&manifest.package.name.as_str()) {
+            continue;
+        }
 
-            * [x]  ensured that my package is licensed such that users can use and distribute the contents of its template directory without restriction, after modifying them through normal use.
-          commit-message: "build: bump @preview/example to 0.1.1"
-          branch: preview/example/v0.1.1
-          base: "main"
-          draft: false
-"###
-    );
+        tasks.push(ArtifactsMatrix {
+            runner: "ubuntu-24.04".to_string(),
+            container: None,
+            package: Package {
+                source: package_dir.display().to_string(),
+                name: manifest.package.name,
+                version: manifest.package.version.to_string(),
+            },
+        });
+    }
+
+    let output = Output {
+        ci: Ci {
+            github: CiGitHub {
+                artifacts_matrix: Some(tasks),
+            },
+        },
+    };
+
+    let stdout = std::io::stdout().lock();
+    let mut writer = BufWriter::new(stdout);
+    serde_json::to_writer_pretty(&mut writer, &output)?;
 
     Ok(())
 }
